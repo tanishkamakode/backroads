1ce6f3838ef04a9a8a6e7697e5ab0c19b92f8f18
Tanishka <makodetanishka@gmail.com>
2024-04-22 21:57:51 +0530

    Add Title component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,14 +1,11 @@
 import React from "react";
+import Title from "./Title";
 
 const Services = () => {
   return (
     <div>
       <section className="section services" id="services">
-        <div className="section-title">
-          <h2>
-            our <span>services</span>
-          </h2>
-        </div>
+        <Title title="Our" subTitle="Services" />
         <div className="section-center services-center">
           <article className="service">
             <span className="service-icon">
