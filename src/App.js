ef7926668c9be3f97c0fce0581fca9a84b97466c
Tanishka <makodetanishka@gmail.com>
2024-04-22 20:17:06 +0530

    Create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1>Backroads App</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
