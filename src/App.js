93acf83b13de377951b0450ff0e6428bca6cd252
Tanishka <makodetanishka@gmail.com>
2024-04-22 21:53:06 +0530

    Fix Hero

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
       <Services />
       <Tours />
       <Footer />
